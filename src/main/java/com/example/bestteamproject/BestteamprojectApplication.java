bee4f6d3954a4dc9af45edce130036d88746cc3e
elhassen <elhacen.eleyatt@smartdev.ai>
2023-12-10 13:39:20 +0000

    First commit on branch starter

---
+++ src/main/java/com/example/bestteamproject/BestteamprojectApplication.java
@@ -0,0 +1,12 @@
+package com.example.bestteamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestteamprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestteamprojectApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/bestteamproject/BestteamprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bestteamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestteamprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
